6fc73a1d3cfac50af8d974e92c703a793d61ecb7
Heruwala <heruwala@gmail.com>
2019-03-23 13:01:11 -0400

    Re-structure project

--- cd/packagingDeployment.sh
+++ cd/packagingDeployment.sh
@@ -15,18 +15,14 @@
 
 #   Note: When you create a Package, also create an extra version before running this script. 
 
-#   To create subsequent versions on your local assuming alias for your DevHub org is MyDevHub, run
-#   scripts/packagingDeployment.sh packaging MyDevHub
+#   To create subsequent versions & install from a developer workstation assuming alias for his/her Scratch org is MyScratchOrg, run
+#   scripts/packagingDeployment.sh packaging MyScratchOrg
 
-#   To install the latest version on your local
-#   developer create version & Install
-#   scripts/packagingDeployment.sh packaging MyDevHub
-
-#   developer install only (already defined in script) to the scratch org
+#   To install latest version from a developer workstation only (already defined in script below) to the scratch org
 #   scripts/packagingDeployment.sh install MyScratchOrg
 
 #   package people create package tag, version & install
-#   scripts/packagingDeployment.sh packaging MyDevHub
+#   scripts/packagingDeployment.sh packaging MyScratchOrg
 
 #   Prod (promote version)
 #   ??
@@ -34,15 +30,16 @@
 #   CD branches install (QA, UAT, Prod)
 #   scripts/packagingDeployment.sh
 
+# Package specific variables
+BUILD_NAME="ECFMG.ES-Base-Objects - CI"
+PACKAGE_NAME="EzSpaceBaseObjects"
+PACKAGE_VERSION="EzSpaceBaseObjects@0.1.0-1"
+
 # Default values
 BRANCH=$1
 SFDX_CLI_EXEC=sfdx
 TARGET_ORG=''
-BUILD_NAME="ECFMG.ES-Base-Objects - CI"
-#TODO: PROJECT_HOME="$BUILD_NAME/package"
-PROJECT_HOME="ECFMG.ES-Base-Objects - CI/package"
-PACKAGE_NAME="EzSpaceBaseObjects"
-PACKAGE_VERSION_BASE_OBJECTS="EzSpaceBaseObjects@0.1.0-1"
+PROJECT_HOME="$BUILD_NAME/package"
 
 if [ "$#" -eq 0 ]; then
   echo "No parameter provided, this will be full package installation to authenticated CD Org"
@@ -68,18 +65,18 @@ if [ $CI ]; then
 fi
 
 # Reading the to be installed package version based on the alias@version key from sfdx-project.json
-PACKAGE_VERSION_BASE_OBJECTS="$(cat sfdx-project.json | jq --arg VERSION "$PACKAGE_VERSION_BASE_OBJECTS" '.packageAliases | .[$VERSION]' | tr -d '"')"
+PACKAGE_VERSION="$(cat sfdx-project.json | jq --arg VERSION "$PACKAGE_VERSION" '.packageAliases | .[$VERSION]' | tr -d '"')"
 
-# We're in a packaging/* branch, so we need to create a new version
-if [ "$BRANCH" = *"packaging"* ]; then
+# We're creating a new version
+if [ $BRANCH = "packaging" ]; then
   echo "Creating new package version for es-base-objects"
-  PACKAGE_VERSION_BASE_OBJECTS="$($SFDX_CLI_EXEC force:package:version:create -p "$PACKAGE_NAME" -x -w 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+  PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGE_NAME -x -w 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
   sleep 300 # We've to wait for package replication.
 fi
 
 # Installation in dependency order
 echo "Package installation es-base-objects"
-$SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION_BASE_OBJECTS -w 10 $TARGET_ORG
+$SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 $TARGET_ORG
 
 #Deleting the Data from records (TODO: fails when record is in use)
 sfdx force:apex:execute -f scripts/my-apex-test.txt
